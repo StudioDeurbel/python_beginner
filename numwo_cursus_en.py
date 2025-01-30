062abfd9a1a5d794b8f4fb037ba076093503904d
StudioDeurbel <nikenaukjeandries@gmail.com>
2025-01-30 20:30:47 +0100

    Add files via upload

---
+++ numwo_cursus_en.py
@@ -0,0 +1,22 @@
+# Hello Roan! I am now giving you a course on writing a Number Word script!
+# Write your answer in another script. Start by creating your variable by writing something simple,
+# then add a space and then =
+example =
+# this will be the number variable. Now add another space, and then int(input("Number: "))
+example = int(input("Number: "))
+# great! Now you have the first line of code! Now add another variable:
+example = int(input("Number: "))
+example2 =
+# now add a second space and then input("Word: ")
+example = int(input("Number: "))
+example2 = input("Word: ")
+# your second line is ready! Now let's go faster! Add this to your third line: for _ in range(*your first variable name*):
+example = int(input("Number: "))
+example2 = input("Word: ")
+for _ in range(example):
+# bam! Third line! Now below print(*your second variable*)
+example = int(input("Number: "))
+example2 = input("Word: ")
+for _ in range(example):
+    print(example2)
+# now execute that code!
